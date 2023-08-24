aaa2e567b91a0a4ebf766572d5ae95472b86c362
skipzero <skip@zerosquadron.com>
2023-08-24 03:30:31 -0700

    changed port and added config stuff

--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
 import express from 'express';
 import cors from 'cors';
-import { config} from ("dotenv")
-import { executeInvoiceOperations } from './invoiceCrus.js';
+import { config} from "dotenv"
+// import { executeInvoiceOperations } from './invoiceCrud';
+import { MongoClient } from 'mongodb';
 
 config();
-console.log('PROCESS!!!', process.env)
+console.log('PROCESS!!!', MongoClient)
+
+async function connectToCluster(uri) {
+  let mongoClient;
+
+  try {
+    mongoClient = new MongoClient(uri);
+    console.log('Connecting to mongodb...', mongoClient);
+
+    await mongoClient.connect();
+    console.log('Successfully connected...');
+
+    return mongoClient;
+  } catch (err) {
+    console.error('connection failed...,', err);
+    process.exit();
+  }
+}
+
+async function executeInvoiceOperations() {
+  const uri = process.env.DB_URI;
+  let mongoClient = new MongoClient(uri)
+
+  try {
+    mongoClient = await connectToCluster(uri);
+    const db = mongoClient.db('FSClusterZed');
+    const collection = db.collection('invoice');
+
+    await createInvoiceDocument(collection);
+  } finally {
+    await mongoClient.close();
+  }
+}
 
 await executeInvoiceOperations();
 
