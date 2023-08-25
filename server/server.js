a21672766dc989f8348382f68e5df40b4f6829fb
skipzero <skip@zerosquadron.com>
2023-08-25 05:19:02 -0700

    tested db call in server...

--- server/server.js
+++ server/server.js
@@ -5,47 +5,45 @@ import { config} from "dotenv"
 import { MongoClient } from 'mongodb';
 
 config();
-console.log('PROCESS!!!', MongoClient)
 
-async function connectToCluster(uri) {
-  let mongoClient;
-
-  try {
-    mongoClient = new MongoClient(uri);
-    console.log('Connecting to mongodb...', mongoClient);
-
-    await mongoClient.connect();
-    console.log('Successfully connected...');
-
-    return mongoClient;
-  } catch (err) {
-    console.error('connection failed...,', err);
-    process.exit();
-  }
-}
-
-async function executeInvoiceOperations() {
-  const uri = process.env.DB_URI;
-  let mongoClient = new MongoClient(uri)
-
-  try {
-    mongoClient = await connectToCluster(uri);
-    const db = mongoClient.db('FSClusterZed');
-    const collection = db.collection('invoice');
-
-    await createInvoiceDocument(collection);
-  } finally {
-    await mongoClient.close();
-  }
-}
-
-await executeInvoiceOperations();
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000 //process.env.PORT || 3000;
 const app = express();
+
+// Connection URL
+const url = process.env.DB_URI;
+// Database Name
+debugger;
+const dbName = 'invoices';
+// Create a new MongoClient
+const client = new MongoClient(url);
+// Connect to the server
+console.log('=======', process.env.DB_URI)
+client.connect(async function (err) {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
+  const db = client.db(dbName);
+  const collection = db.collection('invoice');
+
+  // Find the last record by sorting in descending order and limiting to 1
+  const lastRecord = await collection.find().sort({ _id: -1 }).limit(1).toArray();
+  if (lastRecord.length > 0) {
+    console.log('Last Record:', lastRecord[0]);
+  } else {
+    console.log('No records found.');
+  }
+  // Close the connection
+  client.close();
+});
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/last', (req, res) => {
+  let collection = db.collection('invoice');
+  let q
+})
 
 app.listen(PORT, () => {
   console.log('listening on port', PORT)
